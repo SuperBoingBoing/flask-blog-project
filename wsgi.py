2f1e4088184095d4fdd545250abaa49903bbc4b4
SuperBoingBoing <superboingboing59@gmail.com>
2025-02-17 18:54:43 +0700

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-# Add the path to your project to PYTHONPATH so Python can find your app
-sys.path.append('/home/SuperBoingBoing/flask-blog-project') # 1. Replace with your project path
-
-from app import app # 2. Replace 'app' with the name of your Flask app instance
-
-application = app
